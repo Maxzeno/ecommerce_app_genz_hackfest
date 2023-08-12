4a6054b5de1fee6d873aa866cafd9fb6f4226172
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-08-12 11:48:00 +0100

    added model for products, category, cart, orders etc

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,114 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.utils.html import format_html
+from django_summernote.models import Attachment
+from ckeditor.widgets import CKEditorWidget
+
+# from django_summernote.widgets import SummernoteWidget
+
+from .models import (
+    User, Category, SubCategory, Product, Order, 
+    Email, ContactUs, TransferPayment, Cart, DeliveryPrice, TransferBankAccount
+    )
 
 # Register your models here.
+
+admin.site.site_title = 'Unique Favor Store Admin'
+admin.site.index_title = 'Welcome to Unique Favor Store'
+admin.site.site_header = format_html('<a href="/adminuser/admin/"><img src="/static/img/icon nobg-crop.png" style="height: 100px"></a>')
+
+admin.site.unregister(Group)
+admin.site.unregister(Attachment)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('name', 'email', 'number', 'password', 'description', 'state', 'address', 'image', 'is_superuser', 'is_active', 'is_suspended')
+    list_display = ('name', 'email', 'number', 'is_superuser', 'is_active', 'is_suspended', 'date_joined')
+    order = ('name',)
+    search_fields = ('name', 'email')
+    list_filter = ('is_superuser', 'is_active', 'is_suspended', 'date_joined')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date')
+    search_fields = ('name',)
+    exclude = ('date', 'ordered')
+    list_filter = ('date',)
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category', 'date')
+    search_fields = ('name',)
+    exclude = ('date', 'ordered')
+    list_filter = ('category', 'date')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('id', 'name', 'description', 'state', 'image', 'image2', 'image3', 'sub_category', 'price', 'data', 'is_approved')
+    list_display = ('id', 'name', 'sub_category', 'state', 'price', 'is_approved', 'date')
+    search_fields = ('name','id')
+    exclude = ('ordered',)
+    readonly_fields = ('id',)
+    list_filter = ('is_approved', 'sub_category', 'state', 'date')
+    formfield_overrides = {
+        'RichTextField': {'widget': CKEditorWidget}
+    }
+
+
+# i think i will not register this model so it doesn't show in the admin
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    fields = ('buyer', 'product', 'quantity', 'product_size', 'checked_out', 'price_ordered_at')
+    list_display = ('buyer', 'product', 'quantity', 'product_size', 'checked_out', 'price_ordered_at', 'date')
+    search_fields = ('buyer', 'product', 'quantity')
+    list_filter = ('product', 'buyer', 'quantity', 'checked_out', 'date')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    fields = ('id', 'buyer', 'items', 'paid', 'delivery_fee', 'paystack_ref', 'status', 'has_paid', 'incomplete_payment')
+    list_display = ('id', 'buyer', 'paid', 'paystack_ref', 'delivery_fee', 'status', 'has_paid', 'incomplete_payment', 'date')
+    search_fields = ('id',)
+    readonly_fields = ('id',)
+    list_filter = ('status', 'items', 'has_paid', 'buyer', 'date')
+
+
+@admin.register(TransferPayment)
+class TransferPaymentAdmin(admin.ModelAdmin):
+    fields = ('image', 'buyer', 'order', 'confirmed', 'checked')
+    list_display = ('buyer', 'order', 'confirmed', 'checked', 'date')
+    search_fields = ('buyer',)
+    list_filter = ('confirmed', 'checked', 'buyer', 'date')
+
+
+@admin.register(TransferBankAccount)
+class TransferBankAccountAdmin(admin.ModelAdmin):
+    list_display = ('bank_name', 'bank_number', 'account_name', 'date')
+    search_fields = ('bank_name',)
+    list_filter = ('date',)
+
+
+@admin.register(DeliveryPrice)
+class DeliveryPriceAdmin(admin.ModelAdmin):
+    list_display = ('price', 'date')
+    search_fields = ('price',)
+    list_filter = ('date',)
+
+
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date')
+    search_fields = ('email',)
+    list_filter = ('date',)
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_resolved', 'date')
+    search_fields = ('email',)
+    list_filter = ('is_resolved', 'date')
+
+
--- core/models.py
+++ core/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.apps import apps
 from django.utils import timezone
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db.models import Q
+from django.contrib.auth.hashers import (
+    check_password, is_password_usable, make_password,
+)
+from django.contrib.postgres.fields import JSONField
+from ckeditor.fields import RichTextField
+import shortuuid
+
 
 # Create your models here.
 
@@ -76,3 +85,214 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.name or self.email
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    ordered = models.IntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
+
+    def inc_order(self):
+        self.ordered += 1
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    ordered = models.IntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
+
+    def inc_order(self):
+        self.ordered += 1
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Sub Categories'
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['category', 'name'],
+                name='unique_name_per_category'
+            )
+        ]
+
+        
+def unique_id(model):
+    while True:
+        random = shortuuid.ShortUUID().random(length=8)
+        if not model.objects.filter(id=random).exists():
+            break
+    return random
+
+def product_id():
+    return unique_id(Product)
+
+def order_id():
+    return unique_id(Order)
+
+
+class DeliveryPrice(models.Model):
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateTimeField(default=timezone.now)
+
+
+class TransferBankAccount(models.Model):
+    bank_name = models.CharField(max_length=100)
+    account_name = models.CharField(max_length=250)
+    bank_number = models.CharField(max_length=10)
+    date = models.DateTimeField(default=timezone.now)
+
+
+class Product(models.Model):
+    id = models.CharField(primary_key=True, max_length=10, default=product_id)
+    # data = JSONField() ## will store size and maybe color and quantity for each
+    name = models.CharField(max_length=100)
+    data = models.JSONField(blank=True, null=True)
+    description = RichTextField(blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='images/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='images/', blank=True, null=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    ordered = models.IntegerField(default=0)
+    is_approved = models.BooleanField(default=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    def inc_order(self):
+        self.ordered += 1
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    def is_approved_status(self):
+        if self.is_approved:
+            return 'Approved'
+        return 'Not Approved'
+    
+
+class Cart(models.Model):
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product_size = models.CharField(max_length=100, blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=1)
+    checked_out = models.BooleanField(default=False)
+    date = models.DateTimeField(default=timezone.now)
+    price_ordered_at = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+
+    def paid_check_out(self):
+        self.checked_out = True
+        self.product.inc_order()
+        self.product.sub_category.inc_order()
+        self.product.sub_category.category.inc_order()
+
+        if not self.price_ordered_at:
+            self.price_ordered_at = self.product.price
+        self.save()
+
+    def total_price(self):
+        if self.price_ordered_at:
+            return round(self.price_ordered_at * self.quantity, 2)
+        return round(self.product.price * self.quantity, 2)
+
+    def checked_out_status(self):
+        if self.checked_out:
+            return 'Yes'
+        return 'No'
+
+    def __str__(self):
+        return f"{self.buyer.name} - {self.product.name} x {self.quantity} - checked out: {self.checked_out_status()}"
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('S', 'Success'),
+        ('P', 'Pending'),
+        ('C', 'Cancel'),
+    ]
+
+    id = models.CharField(primary_key=True, max_length=10, default=order_id)
+    paystack_ref = models.CharField(max_length=100, null=True, blank=True)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Cart)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')
+    paid = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    delivery_fee = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    has_paid = models.BooleanField(default=False)
+    incomplete_payment = models.BooleanField(default=False)
+    date = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.has_paid:
+            # i thing we have an issue here which happens when we try to create order from admin
+            print(self.items.all(), 'self.items.all()')
+            for item in self.items.all():
+                item.paid_check_out()
+
+        super().save(*args, **kwargs)
+
+    def get_total_price_now(self):
+        price = 0
+        for item in self.items.all():
+            price += item.total_price()
+        return price
+
+    def order_status(self):
+        for i in self.STATUS_CHOICES:
+            if i[0].upper() == self.status.upper():
+                return i[1]
+        return ''
+
+    def has_paid_status(self):
+        if self.has_paid:
+            return 'Yes'
+        return 'No'
+
+    def __str__(self):
+        return f"{self.id}"
+
+
+class TransferPayment(models.Model):
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    confirmed = models.BooleanField(default=False)
+    checked = models.BooleanField(default=False)
+    date = models.DateTimeField(default=timezone.now)
+
+
+class Email(models.Model):
+    email = models.EmailField(unique=True, null=False)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.email
+    
+
+class ContactUs(models.Model):
+    email = models.EmailField(unique=True)
+    message = models.CharField(max_length=1000)
+    date = models.DateTimeField(default=timezone.now)
+    is_resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name_plural = 'Contact us'
