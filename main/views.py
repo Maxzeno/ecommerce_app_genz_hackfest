158fbf0929b398ecf22be403d7dc9f7d76d16999
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-08-15 21:00:40 +0100

    added products and products detail page

--- main/views.py
+++ main/views.py
@@ -28,6 +28,7 @@ def error_404(request, exception):
 # def error_500(request):
     # return render(request, 'main/500.html', status=500)
 
+
 class Base(View):
 	base_context = {**popular_categories(), 'cart_items': [], 'product_in_cart': []}
 	def get(self, request, *args, **kwargs):
@@ -108,6 +109,73 @@ class ProductDetail(Base):
 			'nav_products': 'green',
 			'btn_disable': btn_disable,
 		})
+
+
+class Products(Base):
+	def get_request(self, request):
+		category_name = request.GET.get('category')
+		sub_name = request.GET.get('sub')
+		search = request.GET.get('search')
+
+		category = None
+		sub_category = None
+		agrs = ''
+
+		has_no_filter_search = False
+
+		if not search:
+
+			if category_name:
+				category = Category.objects.filter(name=category_name).first()
+
+				if sub_name:
+					sub_category = SubCategory.objects.filter(Q(name=sub_name) & Q(category=category)).first()
+
+			if sub_category:
+				agrs += f'&category={category_name}&sub={sub_name}'
+				the_products = Product.objects.filter(Q(sub_category=sub_category) & Q(is_approved=True)).order_by('ordered')
+			elif category:
+				agrs += f'&category={category_name}'
+				the_products = Product.objects.filter(Q(sub_category__category=category) & Q(is_approved=True)).order_by('ordered')
+			else:
+				has_no_filter_search = True
+				the_products = Product.objects.filter(is_approved=True).order_by('ordered')
+		else:
+			the_products = Product.objects.filter(Q(is_approved=True) & Q(name__icontains=search)).order_by('ordered')[:12]
+
+		context = {
+			**paginate_page(the_products, request, Http404, obj='products'),
+			**popular_categories_and_sub(),
+			'number_of_products': len(the_products),
+			'url_args': agrs,
+			'nav_products': 'green',
+			'search_term': search,
+			'has_no_filter_search': has_no_filter_search,
+		}
+
+		return (request, 'main/products.html', context)
+
+
+class ContactUs(Base):
+	def get_request(self, request):
+		form = ContactUsForm()
+		return (request, 'main/contact_us.html', {'form': form, 'nav_contact_us': 'green'})
+
+	def post_request(self, request):
+		form = ContactUsForm(request.POST)
+		if form.is_valid():
+			try:
+				ContactUsModel.objects.create(email=form.cleaned_data.get('email'), message=form.cleaned_data.get('message'))
+				messages.success(request, 'Message has been received')
+				form = ContactUsForm()
+				return (request, 'main/contact_us.html', {'form': form})
+
+			except IntegrityError:
+				messages.warning(request, 'Fill email and message appropriately')
+				return (request, 'main/contact_us.html', {'form': form})
+
+		messages.warning(request, 'Fill email and message appropriately')
+		return (request, 'main/contact_us.html', {'form': form})
 
 
 class EmailSubscribe(View):
